5819c0301d95c8f64b1e8f1e137b87cb3c33de80
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-04-18 09:54:08 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice___Constructors__Methods__and_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
